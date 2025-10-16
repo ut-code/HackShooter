850ae680aff04416db2f1ba387106b7a95814656
Kanata844 <ta3wata@gmail.com>
2025-10-17 00:07:12 +0900

    jarにしても動くよう修正

--- java/HackShooter/src/main/java/net/utcode/Initializer.java
+++ java/HackShooter/src/main/java/net/utcode/Initializer.java
@@ -2,6 +2,7 @@ package net.utcode;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,14 +11,18 @@ import java.nio.file.StandardCopyOption;
 public class Initializer {
     public boolean copyPage(){
         try{
-            Path htmlPath = Paths.get(getClass().getClassLoader().getResource("page/index.html").toURI());
+            InputStream is = getClass().getClassLoader().getResourceAsStream("page/index.html");
             File appDirFile = new File("C:/Users/" + System.getProperty("user.name") + "/HackShooter/");
             if(!appDirFile.exists()){
                 appDirFile.mkdirs();
             }
             Path appDir = Paths.get(appDirFile.toString() + "/index.html");
-            Files.copy(htmlPath, appDir, StandardCopyOption.REPLACE_EXISTING);
-            return true;
+            if(is != null) {
+                Files.copy(is, appDir, StandardCopyOption.REPLACE_EXISTING);
+                return true;
+            } else {
+                return false;
+            }
         }catch (Exception e){
             e.printStackTrace();
             return false;
